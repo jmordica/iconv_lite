1e3fd9a85449209167691bd75bbb759c2fcaf3df
Sail <151263555@qq.com>
2022-05-16 18:14:33 +0800

    first commit

---
+++ mod_test.ts
@@ -0,0 +1,6 @@
+import { assert, assertEquals } from "https://deno.land/std@0.139.0/testing/asserts.ts";
+import * as iconv_lite from "./mod.ts";
+
+Deno.test("iconv_lite", function() {
+  assertEquals("", "");
+});
